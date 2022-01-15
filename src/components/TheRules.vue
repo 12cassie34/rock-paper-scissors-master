4bb03a11a80ecffad46b29e09352e56d00c6453e
Shan Yu Lin <ineslin35@gmail.com>
2022-01-15 16:27:33 +0800

    feat: add the css style of desktop on TheRules component

--- src/components/TheRules.vue
+++ src/components/TheRules.vue
@@ -16,16 +16,31 @@ import TheSignsVue from "./TheSigns.vue";
       w-32
       text-white text-base
       tracking-widest
+      lg:mt-0 lg:mr-8
     "
   >
     <button @click="openRules" href="#rules" class="">RULES</button>
   </div>
   <div
+    @click="closeRules"
+    class="wrapper hidden lg:block"
+    :class="{ 'is-show': showRules }"
+  ></div>
+  <div
     id="rules"
-    class="relative z-20 pt-9 pb-5 h-screen bg-white"
+    class="relative z-20 pt-9 pb-5 h-screen bg-white lg:pt-0 lg:pb-5 lg:pt-8"
     :class="{ 'is-show': showRules }"
   >
-    <h2 class="flex justify-center text-2xl font-extrabold text-background">
+    <h2
+      class="
+        flex
+        justify-center
+        text-2xl
+        font-extrabold
+        text-background
+        lg:justify-start lg:ml-8 lg:text-2xl
+      "
+    >
       RULES
     </h2>
     <img
@@ -35,7 +50,16 @@ import TheSignsVue from "./TheSigns.vue";
       ref="Rules"
     />
 
-    <button @click="closeRules" class="block my-12 mx-auto" href="#">
+    <button
+      @click="closeRules"
+      class="
+        block
+        my-12
+        mx-auto
+        lg:mt-0 lg:mb-0 lg:absolute lg:right-8 lg:top-10
+      "
+      href="#"
+    >
       <img
         :class="{ hidden: !showRules }"
         src="../assets/icon-close.svg"
@@ -56,8 +80,7 @@ export default {
   methods: {
     openRules() {
       this.showRules = true;
-      window.scrollTo({top: 0, behavior: 'smooth'});
-
+      window.scrollTo({ top: 0, behavior: "smooth" });
     },
     closeRules() {
       this.showRules = false;
@@ -77,5 +100,33 @@ export default {
   height: 100vh;
   transform: translateY(-145%);
   transition: all 1s ease-out;
+}
+
+.wrapper.is-show {
+  visibility: visible;
+}
+
+@media (min-width: 1024px) {
+  #rules {
+    transition: none;
+  }
+  #rules.is-show {
+    transform: none;
+    width: 25rem;
+    height: 26rem;
+    position: fixed;
+    top: 7rem;
+    left: 35vw;
+  }
+
+  .wrapper.is-show {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: black;
+    opacity: 0.4;
+  }
 }
 </style>
