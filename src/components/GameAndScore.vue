70a2d44518bbfb83f22d73f1ab90c54daed6750c
Shan Yu Lin <ineslin35@gmail.com>
2022-01-15 16:39:07 +0800

    feat: add the css style of desktop on GameAndScore component

--- src/components/GameAndScore.vue
+++ src/components/GameAndScore.vue
@@ -9,16 +9,19 @@
       border-gray border-2
       rounded-lg
       w-10/12
+      lg:w-2/5
     "
   >
-    <div class="flex items-center text-xl text-white font-semibold">
-      <p class="game-name w-2/4 break-words leading-4">{{ gameName }}</p>
+    <div class="flex items-center text-xl text-white font-semibold lg:text-3xl">
+      <p class="game-name w-2/4 break-words leading-4 lg:leading-7">
+        {{ gameName }}
+      </p>
     </div>
     <div
       class="flex flex-col items-center px-6 py-2 rounded bg-white font-bold"
     >
-      <p class="text-navy text-xs">SCORE</p>
-      <p class="text-dark text-4xl">{{ score }}</p>
+      <p class="text-navy text-xs lg:text-base">SCORE</p>
+      <p class="text-dark text-4xl lg:text-5xl">{{ score }}</p>
     </div>
   </div>
 </template>
